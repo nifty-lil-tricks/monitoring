fd62b8edc95e45fe0e79847d6189798ba4aca419
Jonny Green <hello@jonnydgreen.com>
2023-11-25 18:06:50 +0000

    Make code Deno-first

---
+++ mod.ts
@@ -0,0 +1,4 @@
+// Copyright 2023-2023 the Nifty li'l' tricks authors. All rights reserved. MIT license.
+
+export * from "./decorators/monitor/monitor.ts";
+export * from "./decorators/monitor/monitor.util.ts";
